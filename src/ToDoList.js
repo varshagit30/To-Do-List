56b7e68df003637b6cbeab1181a85498e02663be
varsha <varshavsl.30@gmail.com>
2024-04-23 00:13:47 +0530

    #wip

---
+++ src/ToDoList.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { TiArrowUpThick } from "react-icons/ti";
+import { TiArrowDownThick } from "react-icons/ti";
+
+import "./index.css";
+
+const ToDoList = () => {
+  const [tasks, setTasks] = useState(["Do laundry", "Walk the dog"]);
+  const [newTask, setNewTask] = useState("");
+  const [done, setDone] = useState(false);
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.length > 0) {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+      setDone(false);
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+    console.log(index);
+  };
+
+  const moveTaskUp = (index) => {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  };
+
+  const moveTaskDown = (index) => {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  };
+
+  const handleCheckBoxChange = (index) => {
+    setDone(true);
+  };
+
+  return (
+    <>
+      <h1>To Do List</h1>
+      <input
+        type="text"
+        placeholder="Enter the task..."
+        value={newTask}
+        onChange={handleInputChange}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addTask();
+          }
+        }}
+      />
+      <button className="addBtn" onClick={addTask}>
+        Add
+      </button>
+      <br />
+      {tasks.map((task, index) => (
+        <div className="toDoList">
+          <label>
+            <input
+              className="checkBox"
+              type="checkbox"
+              key={index}
+              // checked={done}
+              // onChange={handleCheckBoxChange}
+            />
+          </label>
+          {/* {console.log("task:   ", task)}
+              {console.log("ind:   ", index.done)} */}
+          <span
+            className="taskText"
+            // style={done ? { textDecoration: "line-through" } : null}
+          >
+            {task}
+          </span>
+          <FaRegTrashAlt className="delBtn" onClick={() => deleteTask(index)} />
+          <TiArrowUpThick className="upBtn" onClick={() => moveTaskUp(index)} />
+          <TiArrowDownThick
+            className="downBtn"
+            onClick={() => moveTaskDown(index)}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ToDoList;
