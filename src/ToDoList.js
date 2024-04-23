403760ccded52eb18f1db1ecc58b3957113c6c3b
varsha <varshavsl.30@gmail.com>
2024-04-24 01:23:24 +0530

    updated checkbox functionality
    #wip

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,9 +6,13 @@ import { TiArrowDownThick } from "react-icons/ti";
 import "./index.css";
 
 const ToDoList = () => {
-  const [tasks, setTasks] = useState(["Do laundry", "Walk the dog", "Clean the room"]);
+  const [tasks, setTasks] = useState([
+    "Do laundry",
+    "Walk the dog",
+    "Clean the room",
+  ]);
   const [newTask, setNewTask] = useState("");
-  const [done, setDone] = useState(false);
+  const [checked, setChecked] = useState([]);
 
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
@@ -18,14 +22,12 @@ const ToDoList = () => {
     if (newTask.length > 0) {
       setTasks((t) => [...t, newTask]);
       setNewTask("");
-      setDone(false);
     }
   };
 
   const deleteTask = (index) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
-    console.log(index);
   };
 
   const moveTaskUp = (index) => {
@@ -50,9 +52,19 @@ const ToDoList = () => {
     } else alert("This task is already at the bottom! ");
   };
 
-  const handleCheckBoxChange = (index) => {
-    setDone(true);
+  const handleCheckBoxChange = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    console.log("event.target.checked", updatedList);
+    setChecked(updatedList);
   };
+
+  const isChecked = (item) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
 
   return (
     <>
@@ -79,18 +91,12 @@ const ToDoList = () => {
               className="checkBox"
               type="checkbox"
               key={index}
-              // checked={done}
-              // onChange={handleCheckBoxChange}
+              value={task}
+              checked={checked.includes(task)}
+              onChange={handleCheckBoxChange}
             />
           </label>
-          {/* {console.log("task:   ", task)}
-              {console.log("ind:   ", index.done)} */}
-          <span
-            className="taskText"
-            // style={done ? { textDecoration: "line-through" } : null}
-          >
-            {task}
-          </span>
+          <span className={isChecked(task)}>{task}</span>
           <FaRegTrashAlt
             className="delBtn"
             onClick={() => deleteTask(index)}
